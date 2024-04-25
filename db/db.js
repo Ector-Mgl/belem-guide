37c0582e39c018f881ff53c94d28dc4a404389e3
Ector <ectorjobs1@gmail.com>
2024-04-24 23:05:20 -0300

    second-commit

--- db/db.js
+++ db/db.js
@@ -3,12 +3,7 @@ const dotenv = require('dotenv');
 
 dotenv.config({ path: 'C:/Users/Erika/Desktop/apigooglw/login_auth/.env' });
 
-const db = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "password",
-    database: "clientuser"
-});
+const db = mysql.createConnection("mysql://momy5bktd24w0xqt:q0tdztpnzx9qbiqd@i5x1cqhq5xbqtv00.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/xz93eq2c3k1o4qah");
 
 db.connect((error) => {
     if (error) {
